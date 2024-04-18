e8eacda95ced0c199124d071162f95d3cee6e96c
Aftabul Islam <toaihimel@gmail.com>
2024-04-18 21:58:40 +0600

    feat: Composer init
    feature/plugin-init

---
+++ shipping-manager.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Shipping Manager
+ * Description: A simplified all in one shipping solution for WooCommerce.
+ * Version: 1.00.0
+ * Requires Plugins: woocommerce
+ */
+
+defined('ABSPATH') || exit; // Security check
+
+require_once "vendor/autoload.php";
