1c25dd39ab9b95e2dc200b0bd05676c1729688a0
codingspider <engrokon.rok@gmail.com>
2024-05-08 12:15:15 +0600

    add home page and add css file

--- inc/Assets.php
+++ inc/Assets.php
@@ -11,6 +11,7 @@ defined( 'ABSPATH' ) || exit;
 class Assets {
 	const INSTANCE_KEY = 'Assets';
 	const ADMIN_SHIPPING_SETTINGS_SCRIPT = 'tsm_admin_shipping_settings_script';
+	const ADMIN_SHIPPING_SETTINGS_STYLE = 'tsm_admin_shipping_settings_style';
 
 	/**
 	 * Initializes the object
@@ -19,6 +20,7 @@ class Assets {
 	 */
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', [ $this, 'register_admin_scripts' ] );
+		add_action('admin_enqueue_scripts', [ $this, 'register_admin_styles' ]);
 	}
 
 	/**
@@ -31,5 +33,14 @@ class Assets {
 			self::ADMIN_SHIPPING_SETTINGS_SCRIPT,
 			TSM_ROOT_FOLDER_URL . 'assets/js/admin/shipping-settings.js'
 		);
+
+	}
+
+	function register_admin_styles() {
+		wp_enqueue_style(
+			self::ADMIN_SHIPPING_SETTINGS_STYLE,
+			TSM_ROOT_FOLDER_URL . 'assets/css/admin/shipping-settings.css'
+		);
 	}
 }
+
--- templates/home.php
+++ templates/home.php
@@ -1,4 +1,38 @@
 <?php
 defined( 'ABSPATH' ) || exit;
 ?>
-Home.php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-3 left-menu">
+            <div>
+                <h2>LOGO</h2>
+            </div>
+        </div>
+        <div class="col-9 main">
+            <div class="row">
+                <div class="col-4 ">
+                    <div class="first-box">
+                        <h2>Rule Title</h2>
+                        <p>Lorem ipsum and more about the details of the rules</p>
+                        <a href="&sub_page=add">Edit</a>
+                    </div>
+                </div>
+                <div class="col-4 ">
+                    <div class="add-box">
+                        <a href="&sub_page=add">ADD</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
