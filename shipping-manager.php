a6e61fc4ebb5d68a0ec5daf55f1745e7bce6638b
Aftabul Islam <toaihimel@gmail.com>
2024-06-11 15:58:45 +0600

    fix: phpcs issue fix

--- shipping-manager.php
+++ shipping-manager.php
@@ -3,6 +3,7 @@
  * Plugin Name: Shipping Manager
  * Version: 1.0.0
  * Description: An simple plugin to manage your shipping
+ * Text Domain: shipping-manager
  */
 
 require 'vendor/autoload.php';
