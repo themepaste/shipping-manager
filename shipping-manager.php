bb3ba98cad6c0e8eedfdbaa5290d852cd4f8c07a
Aftabul Islam <toaihimel@gmail.com>
2024-06-11 15:37:35 +0600

    feat: Main plugin file added

--- shipping-manager.php
+++ shipping-manager.php
@@ -1,19 +1,7 @@
 <?php
 /**
  * Plugin Name: Shipping Manager
- * Description: A simplified all in one shipping solution for WooCommerce.
- * Version: 1.00.0
- * Requires Plugins: woocommerce
- *
- * Requires PHP: 7.2
- * Text Domain: tsm-shipping-manager
+ * Version: 1.0.0
  */
 
-defined('ABSPATH') || exit; // Security check
-
-defined( 'TSM_ROOT_FILE_PATH' ) || define( 'TSM_ROOT_FILE_PATH', __DIR__ );
-defined( 'TSM_ROOT_FOLDER_URL' ) || define( 'TSM_ROOT_FOLDER_URL', plugin_dir_url( __FILE__ ) );
-
-require_once "vendor/autoload.php";
-
-\Themepaste\ShippingManager\ShippingManager::init();
+require 'vendor/autoload.php';
