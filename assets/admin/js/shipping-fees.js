1da02435f1667067fbf9932a07e63d3982a3f790
Aftabul Islam <toaihimel@gmail.com>
2024-07-24 13:23:47 +0600

    fix: Assets logged
    wp-org-refinement

---
+++ assets/admin/js/shipping-fees.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************************!*\
+  !*** ./src/js/shipping-fees.js ***!
+  \*********************************/
+(function ($) {
+  $(document).ready(function () {});
+})(jQuery);
+/******/ })()
+;
+//# sourceMappingURL=shipping-fees.js.map
