2d5e2e3abaa79f6addb11635200c4af4cc0beb39
mrinal013 <mrinalhaque99@gmail.com>
2024-11-02 20:06:22 +0600

    made processing amount conditional to add processing fee checkbox, added .eslintrc, added wp-prettier package

--- assets/admin/js/shipping-fees.js
+++ assets/admin/js/shipping-fees.js
@@ -1,11 +1,12 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*********************************!*\
-  !*** ./src/js/shipping-fees.js ***!
-  \*********************************/
-(function ($) {
-  $(document).ready(function () {});
-})(jQuery);
-/******/ })()
-;
-//# sourceMappingURL=shipping-fees.js.map
+( () => {
+	const e = '#enable-processing-fees',
+		n = 'medium';
+	! ( function ( c ) {
+		function o() {
+			const o = c( e ),
+				s = c( '#processing-fees-amount' ).closest( '.input-wrapper' );
+			o.is( ':checked' ) ? s.show( n ) : s.hide( n );
+		}
+		c( document ).ready( function () {} ), c( e ).on( 'change', o ), o();
+	} )( jQuery );
+} )();
--- src/js/shipping-fees.js
+++ src/js/shipping-fees.js
@@ -1,3 +1,34 @@
-(function($){$(document).ready(function(){
+/**
+ * Admin settings script for shipping fees page
+ *
+ * @since 1.1.0
+ */
+const INPUT_WRAPPER = '.input-wrapper';
+const PROCESSING_FEE_ID = '#enable-processing-fees';
+const PROCESSING_FEE_AMOUNT_ID = '#processing-fees-amount';
+const DISPLAY_TYPE = 'medium';
 
-})})(jQuery)
+( function ( $ ) {
+	$( document ).ready( function () {} );
+
+	/**
+	 * Minimum amount toggle
+	 *
+	 * @since 1.2.1
+	 */
+	function processingFeeAmount() {
+		const processingFeeEnableField = $( PROCESSING_FEE_ID );
+		const processingFeeAmountWrapper = $(
+			PROCESSING_FEE_AMOUNT_ID
+		).closest( INPUT_WRAPPER );
+
+		if ( processingFeeEnableField.is( ':checked' ) ) {
+			processingFeeAmountWrapper.show( DISPLAY_TYPE );
+		} else {
+			processingFeeAmountWrapper.hide( DISPLAY_TYPE );
+		}
+	}
+
+	$( PROCESSING_FEE_ID ).on( 'change', processingFeeAmount );
+	processingFeeAmount();
+} )( jQuery );
