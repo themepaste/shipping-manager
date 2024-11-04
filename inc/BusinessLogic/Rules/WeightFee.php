c31f18dee354ea24cd2ae5be43f8fdf1684f1985
mrinal013 <mrinalhaque99@gmail.com>
2024-11-04 13:05:41 +0600

    added weight based range shipping cost

--- inc/BusinessLogic/Rules/WeightFee.php
+++ inc/BusinessLogic/Rules/WeightFee.php
@@ -35,21 +35,27 @@ class WeightFee extends AbstractRules implements RulesInterface {
 				$fee_per_unit = $shipping_fees->get( ShippingFeesSettings::WEIGHT_BASED_PER_UNIT_AMOUNT_FEES );
 				foreach( $this->package['contents'] as $product_key => $product_data ) {
 					$quantity = $product_data['quantity'];
-					$weight = $product_data['data']->get_weight();
-					$total_weight = $quantity * (float) $weight;
-					$cost += $total_weight * $fee_per_unit;
+					$cost += $quantity * $fee_per_unit;
 				}
+				
 			}
 
 			if ( $fee_type === 'weight-range-unit' ) {
 				$fee_range_unit_rules = $shipping_fees->get( ShippingFeesSettings::WEIGHT_BASED_RANGE_UNIT_RULES );
 				$total_weight = 0;
-				foreach( $this->package['contents'] as $product_key => $product_data ) {
+				foreach ( $this->package['contents'] as $product_key => $product_data ) {
 					$weight = $product_data['data']->get_weight();
 					$total_weight += (float) $weight;
 				}
-				error_log( print_r( $total_weight, true ));
-				// $cost += $total_weight * $fee_per_unit;
+				foreach ( $fee_range_unit_rules as $key => $value ) {
+					$from = isset( $value[0] ) ? $value[0] : 0;
+					$to = isset( $value[1] ) ? $value[1] : INF;
+					$fee = isset( $value[2] ) ? $value[2] : 0;
+					if ( ( $total_weight >= $from ) && ( $total_weight <= $to ) ) {
+						$cost += (float) $fee;
+						break;
+					}
+				}
 			}
 		}
 		return $cost;
