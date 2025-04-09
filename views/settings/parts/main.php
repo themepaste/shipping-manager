b19479043e00063eafeac869f1bc265a9b9ea19f
byteshake-habib <habib@themepaste.com>
2025-04-09 14:41:06 +0600

    routeing added

--- views/settings/parts/main.php
+++ views/settings/parts/main.php
@@ -1,5 +1,15 @@
-<?php defined( 'ABSPATH' ) || exit; ?>
+<?php
+
+use ThemePaste\ShippingManager\Helpers\Utility;
+
+ defined( 'ABSPATH' ) || exit; ?>
 
 <div class="tpsm-main-wrapper">
-    <h1>Main</h1>
+    <?php 
+        foreach ( $settings_option as $key => $value ) {
+            if( $current_screen == $key ) {
+                echo Utility::get_template( 'settings/pages/' . $key . '.php' );
+            }
+        }
+    ?>
 </div>
