30c6bc9a152a09fa2a5e21f673d40936f48b7851
byteshake-habib <habib@themepaste.com>
2025-04-10 15:34:14 +0600

    misc

--- views/settings/pages/shipping-fees.php
+++ views/settings/pages/shipping-fees.php
@@ -1,8 +1,15 @@
 <?php defined( 'ABSPATH' ) || exit; ?>
 
 <div class="tpsm-shipping-fees-wrapper">
-    <div class="tpsm-setting-row">
-        <label>Disable/Enable:</label>
-        <input class="tpsm-switch" type="checkbox" id="tpsm-shipping-fees-disable-enable" /><label for="tpsm-shipping-fees-disable-enable" class="tpsm-switch-label">Toggle</label>
-    </div>
+    <form action="">
+        <div class="tpsm-setting-row">
+            <label>Disable/Enable:</label>
+            <input class="tpsm-switch" type="checkbox" id="tpsm-shipping-fees-disable-enable" /><label for="tpsm-shipping-fees-disable-enable" class="tpsm-switch-label">Toggle</label>
+        </div>
+
+        <div class="tpsm-save-button">
+            <button type="submit">Save</button>
+        </div>
+    </form>
+    
 </div>
