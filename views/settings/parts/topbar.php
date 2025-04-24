d29b762d6cd701fcecaf20e46c0d1c09bf0f0852
byteshake-habib <habib@themepaste.com>
2025-04-24 13:26:56 +0600

    top bar added

--- views/settings/parts/topbar.php
+++ views/settings/parts/topbar.php
@@ -1,4 +1,18 @@
 <?php defined( 'ABSPATH' ) || exit; ?>
-<h1><?php esc_html_e( 'Shipping Manager', 'shipping-manager' ); ?></h1>
-<p style="margin:0;"><?php esc_html_e( 'Set up global shipping rules', 'shipping-manager' ); ?></p>
-<br>
+<div class="tpsm-plugin-topbar-wrapper">
+    <div class="tpsm-logo-title-area">
+        <div class="tpsm-icons">
+            <!-- <img src="<?php echo TPSM_ASSETS_URL . '/img/icons/supply-chain-management.png' ?>" alt="Shipping Manager Icons"> -->
+            <img src="<?php echo TPSM_ASSETS_URL . '/img/icons/order-fulfillment.png' ?>" alt="Shipping Manager Icons">
+        </div>
+        <div class="tpsm-titles">
+            <h1><?php esc_html_e( 'Shipping Manager', 'shipping-manager' ); ?></h1>
+            <p style="margin:0;"><?php esc_html_e( 'Set up your store global shipping rules', 'shipping-manager' ); ?></p>
+        </div>
+    </div>
+    <div class="tpsm-topbar-info-area">
+        <a href="#">Documentation</a>
+    </div>
+</div>
+    
+    <br>
