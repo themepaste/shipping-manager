ac307b267041e844de5c308a34dca8373eecc50a
byteshake-habib <habib@themepaste.com>
2025-04-28 16:43:46 +0600

    general setting value saved

--- views/settings/pages/general.php
+++ views/settings/pages/general.php
@@ -7,7 +7,6 @@
     $submit_button      = $prefix . '-' . $screen_slug . '_submit';
     $option_name        = $prefix . '-' . $screen_slug . '_' . 'settings';
     $saved_settings     = get_option( $option_name );
-    $parent_field_key   = 'minimum-amount'; //It has a child field called "Cart Amount"
 
     $general_settings_fields = [
         'method-title' => array(
@@ -40,6 +39,11 @@
 
             <?php 
                 foreach ( $general_settings_fields as $key => $field ) {
+
+                    if( isset( $saved_settings ) && ! empty( $saved_settings ) ) {
+                        $field['value'] = isset( $saved_settings[ $key ] ) ? $saved_settings[ $key ] : '';
+                    }
+
                     if( 'text' == $field['type'] ) {
                         printf(
                             '<div class="tpsm-setting-row">
@@ -49,14 +53,62 @@
                                     </div>
                                     <div class="tpsm-field-input">
                                         <div>
-                                            <input type="text" type="text" >
+                                            <input type="text" type="text" name="%3$s" value="%4$s">
                                         </div>
                                         <p class="tpsm-field-desc">%2$s</p>
                                     </div>
                                 </div>
                             </div>',
-                            $field['label'],
-                            $field['desc']
+                            $field['label'],                                //%1$s =  Field Label
+                            $field['desc'],                                 //%2$s =  Description
+                            $prefix . '-' . $screen_slug . '_' . $key,      //%3$s =  Field Name
+                            $field['value'],                                //%4$s =  Value
+                        );
+                    }
+                    else if( 'switch' == $field['type'] ) {
+                        printf(
+                            '<div class="tpsm-setting-row">
+                                <div class="tpsm-field">
+                                    <div class="tpsm-field-label">
+                                        <label>%1$s:</label>
+                                    </div>
+                                    <div class="tpsm-field-input">
+                                        <div class="tpsm-switch-wrapper">
+                                            <input class="tpsm-switch" type="checkbox" id="%3$s" name="%3$s" %4$s /><label for="%3$s" class="tpsm-switch-label"></label>
+                                        </div>
+                                        <p class="tpsm-field-desc">%2$s</p>
+                                    </div>
+                                </div>
+                            </div>',
+                            $field['label'],                            //%1$s =  Field Label
+                            $field['desc'],                             //%1$s =  Description
+                            $prefix . '-' . $screen_slug . '_' . $key,  //%1$s =  Field name
+                            $field['value'] == 1 ? 'checked' : '',      //%1$s =  Value
+                        );
+                    }
+                    else if( 'select' == $field['type'] ) {
+                        printf(
+                            '<div class="tpsm-setting-row">
+                                <div class="tpsm-field">
+                                    <div class="tpsm-field-label">
+                                        <label>%1$s: </label>
+                                    </div>
+                                    <div class="tpsm-field-input">
+                                        <div class="tpsm-select-wrapper">
+                                            <select name="%3$s" id="%3$s">
+                                                <option value="no" %4$s >No</option>
+                                                <option value="yes" %4$s >Yes</option>
+                                            </select>
+                                        </div>
+                                        <p class="tpsm-field-desc">%2$s</p>
+                                    </div>
+                                </div>
+                            </div>',
+                            $field['label'],                                //%1$s =  Field Label
+                            $field['desc'],                                 //%2$s =  Description
+                            $prefix . '-' . $screen_slug . '_' . $key,      //%3$s =  Field Name
+                            $field['value'] == 'no' ? 'selected' : '',      //%4$s =  Value if no
+                            $field['value'] == 'yes' ? 'selected' : '',     //%4$s =  Value if yes
                         );
                     }
                 }
@@ -65,33 +117,10 @@
             <div class="tpsm-setting-row">
                 
                 <!-- Switch for enable disable  -->
-                <div class="tpsm-field">
-                    <div class="tpsm-field-label">
-                        <label><?php esc_html_e( 'Disable/Enable:', 'shipping-manager' ); ?></label>
-                    </div>
-                    <div class="tpsm-field-input">
-                        <div class="tpsm-switch-wrapper">
-                            <input class="tpsm-switch" type="checkbox" id="tpsm-box-shipping-disable-enable" name="tpsm-box-shipping-disable-enable" /><label for="tpsm-box-shipping-disable-enable" class="tpsm-switch-label"></label>
-                        </div>
-                        <p class="tpsm-field-desc"><?php esc_html_e( 'To enable/disable method. By deafult enabled', 'shipping-manager' ); ?></p>
-                    </div>
-                </div>
+                
                 
                 <!-- Taxable Field  -->
-                <div class="tpsm-field">
-                    <div class="tpsm-field-label">
-                        <label><?php esc_html_e( 'Taxable: ', 'shipping-manager' ); ?></label>
-                    </div>
-                    <div class="tpsm-field-input">
-                        <div class="tpsm-select-wrapper">
-                            <select name="" id="">
-                                <option value="no"><?php esc_html_e( 'No', 'shipping-manager' ); ?></option>
-                                <option value="yes"><?php esc_html_e( 'Yes', 'shipping-manager' ); ?></option>
-                            </select>
-                        </div>
-                        <p class="tpsm-field-desc"><?php esc_html_e( 'Will shipping method taxable or not', 'shipping-manager' ); ?></p>
-                    </div>
-                </div>
+                
 
             </div>
 
@@ -101,3 +130,50 @@
         </form>
     </div>
 </div>
+
+
+<?php 
+    /**
+     * Proccessing the form 
+     * 
+     * Save Free Shipping Setting option
+     */
+    if( isset( $_POST[$submit_button] ) ) {
+
+        if ( ! isset( $_POST['tpsm-nonce_name'] ) || ! wp_verify_nonce( $_POST['tpsm-nonce_name'], 'tpsm-nonce_action' ) ) {
+            wp_die( __( 'Nonce verification failed.', 'shipping-manager' ) );
+        }
+    
+        // Check capabilities if needed
+        if ( ! current_user_can( 'manage_options' ) ) {
+            wp_die( __( 'Unauthorized user', 'shipping-manager' ) );
+        }
+
+        $settings_values = [];
+
+        foreach ( $general_settings_fields as $key => $field ) {
+            $field_name = $prefix . '-' . $screen_slug . '_' . $key;
+
+            if( 'switch' == $field['type'] ) {
+                $settings_values[$key] = isset( $_POST[$field_name] ) ? 1 : 0;
+            }
+            else if( 'text' == $field['type'] ) {
+                $settings_values[$key] = isset( $_POST[$field_name] ) ? sanitize_text_field( $_POST[$field_name] ) : '';
+            }
+            else if( 'select' == $field['type'] ) {
+                $settings_values[$key] = isset( $_POST[$field_name] ) ? sanitize_text_field( $_POST[$field_name] ) : '';
+            }
+        }
+
+        update_option( $option_name, $settings_values );
+
+        wp_redirect( add_query_arg( 
+            array(
+                'page'          => 'shipping-manager',
+                'tpsm-setting'  => $screen_slug,
+            ),
+            admin_url( 'admin.php' )
+        ) );
+
+        exit;
+    }
