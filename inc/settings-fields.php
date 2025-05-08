ab9f481998f52ed8a68a6a6fe8577fade7516244
byteshake-habib <habib@themepaste.com>
2025-05-08 16:17:19 +0600

    settings fileds poper documentation added

--- inc/settings-fields.php
+++ inc/settings-fields.php
@@ -1,130 +1,150 @@
-<?php 
+<?php
+/**
+ * Shipping Manager Settings Fields
+ *
+ * @package ShippingManager
+ */
 
 defined( 'ABSPATH' ) || exit;
 
-if( ! function_exists( 'tpsm_general_settings_fields' ) ) {
+if ( ! function_exists( 'tpsm_general_settings_fields' ) ) {
+    /**
+     * General Settings Fields
+     *
+     * @return array
+     */
     function tpsm_general_settings_fields() {
-        return [
+        return array(
             'method-title' => array(
                 'label' => __( 'Method Title', 'shipping-manager' ),
                 'type'  => 'text',
                 'value' => '',
-                'desc'  => __( 'By default "Shipping Manager"', 'shipping-manager' ),
+                'desc'  => __( 'By default: "Shipping Manager"', 'shipping-manager' ),
             ),
             'is-plugin-enable' => array(
                 'label' => __( 'Disable/Enable', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'To enable/disable method. By deafult enabled', 'shipping-manager' ),
+                'desc'  => __( 'Enable or disable the shipping method. Default: enabled.', 'shipping-manager' ),
             ),
             'is-plugin-taxable' => array(
                 'label' => __( 'Taxable', 'shipping-manager' ),
                 'type'  => 'select',
                 'value' => '',
-                'desc'  => __( 'Will shipping method taxable or not', 'shipping-manager' ),
+                'desc'  => __( 'Specify if the shipping method is taxable.', 'shipping-manager' ),
             ),
-        ];
+        );
     }
 }
 
-if( ! function_exists( 'tpsm_free_shipping_settings_fields' ) ) {
+if ( ! function_exists( 'tpsm_free_shipping_settings_fields' ) ) {
+    /**
+     * Free Shipping Settings Fields
+     *
+     * @return array
+     */
     function tpsm_free_shipping_settings_fields() {
-        return [
+        return array(
             'hide-other' => array(
                 'label' => __( 'Hide Other', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'Hide other shipping methods while free shipping is available.', 'shipping-manager' ),
-            ), 
+                'desc'  => __( 'Hide other shipping methods when free shipping is available.', 'shipping-manager' ),
+            ),
             'minimum-amount' => array(
                 'label' => __( 'Minimum Amount', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'This will enable a custom minimum amount for free shipping. Otherwise it will use default minimum amount set in WooCommerce free shipping.', 'shipping-manager' ),
+                'desc'  => __( 'Enable custom minimum amount for free shipping. Otherwise, the default WooCommerce setting is used.', 'shipping-manager' ),
             ),
             'cart-amount' => array(
                 'label' => __( 'Cart Amount', 'shipping-manager' ),
                 'type'  => 'text',
                 'value' => '',
-                'desc'  => __( 'Cart minimum amount for free shipping.', 'shipping-manager' ),
-            ), 
+                'desc'  => __( 'Cart minimum amount required for free shipping.', 'shipping-manager' ),
+            ),
             'free-shipping-bar' => array(
                 'label' => __( 'Show Free Shipping Progress Bar', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'Enable free shipping bar for customers. Customers will see a bar for target to achieve free shipping.', 'shipping-manager' ),
-                'child-fields' => [
+                'desc'  => __( 'Enable a progress bar showing customers their progress towards free shipping.', 'shipping-manager' ),
+                'child-fields' => array(
                     'shipping-bar-message' => array(
                         'label' => __( 'Message', 'shipping-manager' ),
                         'type'  => 'text',
                         'value' => '',
-                        'desc'  => __( 'To show how many prie left to qualify free shipment than please use [left_price] as placeholder.', 'shipping-manager' ),
+                        'desc'  => __( 'Use [left_price] as a placeholder for the remaining amount to qualify for free shipping.', 'shipping-manager' ),
                     ),
                     'shipping-bar-position' => array(
-                        'label' => __( 'Position', 'shipping-manager' ),
-                        'type'  => 'select',
-                        'value' => '',
-                        'desc'  => __( 'Position', 'shipping-manager' ),
-                        'options' => [
-                            'Bottom',
-                            'Top'
-                        ]
+                        'label'   => __( 'Position', 'shipping-manager' ),
+                        'type'    => 'select',
+                        'value'   => '',
+                        'desc'    => __( 'Position of the progress bar.', 'shipping-manager' ),
+                        'options' => array(
+                            'Bottom' => __( 'Bottom', 'shipping-manager' ),
+                            'Top'    => __( 'Top', 'shipping-manager' ),
+                        ),
                     ),
                     'shipping-bar-alignment' => array(
-                        'label' => __( 'Alignment', 'shipping-manager' ),
-                        'type'  => 'select',
-                        'value' => '',
-                        'desc'  => __( 'Alignment', 'shipping-manager' ),
-                        'options' => [
-                            'Left',
-                            'Center',
-                            'Right'
-                        ]
+                        'label'   => __( 'Alignment', 'shipping-manager' ),
+                        'type'    => 'select',
+                        'value'   => '',
+                        'desc'    => __( 'Alignment of the progress bar.', 'shipping-manager' ),
+                        'options' => array(
+                            'Left'   => __( 'Left', 'shipping-manager' ),
+                            'Center' => __( 'Center', 'shipping-manager' ),
+                            'Right'  => __( 'Right', 'shipping-manager' ),
+                        ),
                     ),
                     'shipping-bar-text-color' => array(
                         'label' => __( 'Text Color', 'shipping-manager' ),
                         'type'  => 'picker',
                         'value' => '',
-                        'desc'  => __( 'Text color', 'shipping-manager' ),
+                        'desc'  => __( 'Text color of the progress bar.', 'shipping-manager' ),
                     ),
                     'shipping-bar-background-color' => array(
                         'label' => __( 'Background Color', 'shipping-manager' ),
                         'type'  => 'picker',
                         'value' => '',
-                        'desc'  => __( 'Background Color', 'shipping-manager' ),
+                        'desc'  => __( 'Background color of the progress bar.', 'shipping-manager' ),
                     ),
-                ]
+                ),
             ),
-        ];
+        );
     }
 }
 
-if( ! function_exists( 'tpsm_shipping_calculator_settings_fields' ) ) {
+if ( ! function_exists( 'tpsm_shipping_calculator_settings_fields' ) ) {
+    /**
+     * Shipping Calculator Settings Fields
+     *
+     * @return array
+     */
     function tpsm_shipping_calculator_settings_fields() {
-        return [
+        return array(
             'shipping-calculator-enable' => array(
                 'label' => __( 'Disable/Enable', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'To enable/disable features. By deafult enabled', 'shipping-manager' ),
+                'desc'  => __( 'Enable or disable the shipping calculator. Default: enabled.', 'shipping-manager' ),
             ),
             'enable-location-field' => array(
-                'label' => __( 'Off Location form', 'shipping-manager' ),
+                'label' => __( 'Off Location Form', 'shipping-manager' ),
                 'type'  => 'switch',
                 'value' => '',
-                'desc'  => __( 'To enable/disable features. By deafult enabled', 'shipping-manager' ),
+                'desc'  => __( 'Enable or disable the location form. Default: enabled.', 'shipping-manager' ),
             ),
             'shipping-calculator-position' => array(
-                'label' => __( 'Position', 'shipping-manager' ),
-                'type'  => 'select',
-                'value' => '',
-                'desc'  => __( 'Placehment of shipping calculator', 'shipping-manager' ),
-                'options' => [
-                    'before-add-to-cart-button' => 'Before Add to Cart buton',
-                    'after-add-to-cart-button'  => 'After Add to Cart buton',
-                    'using-shortcode'           => 'Using Shortcode',
-                ],
+                'label'   => __( 'Position', 'shipping-manager' ),
+                'type'    => 'select',
+                'value'   => '',
+                'desc'    => __( 'Placement of the shipping calculator.', 'shipping-manager' ),
+                'options' => array(
+                    'before-add-to-cart-button' => __( 'Before Add to Cart button', 'shipping-manager' ),
+                    'after-add-to-cart-button'  => __( 'After Add to Cart button', 'shipping-manager' ),
+                    'using-shortcode'           => __( 'Using Shortcode', 'shipping-manager' ),
+                ),
             ),
-        ];
+        );
     }
 }
