8eb20a424855e2e5f1c645cb344fc53b92b098da
byteshake-habib <habib@themepaste.com>
2025-05-11 16:24:05 +0600

    guide me details hide

--- views/settings/parts/guide-me.php
+++ views/settings/parts/guide-me.php
@@ -3,12 +3,3 @@
 ?>
 
 <button class="tpsm-guide-me-button" id="tpsm-guide-me-button"><?php esc_html_e( 'Guide Me', 'shipping-manager' ); ?></button>
-<div class="tpsm-guide-me-container-wrapper">
-    <div class="tpsm-guide-me-container">
-        <h2><?php esc_html_e('Click the documentation link or watch the video to learn more about this feature.', 'shipping-manager' ) ?></h2>
-        <button><a href="#"><?php esc_html_e( 'Documentation', 'shipping-manager' ); ?></a></button>
-        <iframe src="https://www.youtube.com/embed/yVYQeDhAQWk?si=sPacxkWaGkT_g1jH" allowfullscreen></iframe>
-    
-        <span class="tpsm-guide-me-cross" id="tpsm-guide-me-cross"><?php echo esc_html( '&#10005;' ); ?></span>
-    </div>
-</div>
