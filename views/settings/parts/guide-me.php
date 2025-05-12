280370bb8152111c853b072e8681d16ea81e1633
byteshake-habib <habib@themepaste.com>
2025-05-12 10:49:34 +0600

    guide me button doc conditionally added

--- views/settings/parts/guide-me.php
+++ views/settings/parts/guide-me.php
@@ -1,5 +1,26 @@
 <?php 
     defined( 'ABSPATH' ) || exit;
-?>
+    $current_screen = $args['current_screen'];
 
-<button class="tpsm-guide-me-button" id="tpsm-guide-me-button"><?php esc_html_e( 'Guide Me', 'shipping-manager' ); ?></button>
+switch ( $current_screen ) {
+    case 'shipping-fees':
+        $doc_url = 'shipping-fees';
+        break;
+    case 'box-shipping':
+        $doc_url = 'box-shipping';
+        break;
+    case 'free-shipping':
+        $doc_url = 'free-shipping';
+        break;
+    case 'shipping-calculator':
+        $doc_url = 'shipping-calculator';
+        break;
+    case 'general':
+        $doc_url = 'general';
+        break;
+    default:
+        $doc_url = 'https://themepaste.com/documentation/shipping-manager-documentation';
+        
+}
+?>
+<button class="tpsm-guide-me-button" id="tpsm-guide-me-button"><a href="<?php echo esc_url( $doc_url ); ?>"><?php esc_html_e( 'Guide Me', 'shipping-manager' ); ?></a></button>
