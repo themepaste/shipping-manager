16422268f241ebde7f874f9b499f82f578b0d0d1
byteshake-habib <habib@themepaste.com>
2025-06-18 13:32:46 +0600

    misc

--- app/Traits/Data.php
+++ app/Traits/Data.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) || exit;
 trait Data {
     protected $conditions_data = [
         'always'        => 'Always',
-        'total-price'   => 'Total Price',
-        'weight'        => 'Weight',
+        'total-price'   => 'Cart Total Price',
+        'weight'        => 'Total Weight',
     ];
 }
