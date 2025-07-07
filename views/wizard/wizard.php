06d5a25ad296586cd62ffaefedbaa297db0f69ce
byteshake-habib <habib@themepaste.com>
2025-07-07 15:12:45 +0600

    setup wizard design done

--- views/wizard/wizard.php
+++ views/wizard/wizard.php
@@ -14,7 +14,7 @@ defined( 'ABSPATH' ) || exit;
         <p>By opting in, you’ll get notifications about important security patches, new features, helpful tips, and occasional special offers. We’ll also collect a bit of basic information about your WordPress setup. This helps us improve compatibility with your site and ensures the plugin works more effectively for your needs.</p>
         <form action="" method="post">
             <button type="submit" value="0" class="button button-secondary"><?php esc_html_e( 'Not now', 'shipping-manager' ); ?></button>
-            <button type="submit" value="1" class="active button button-primary"><?php esc_html_e( 'Continue and stay updated', 'shipping-manager' ); ?></button>
+            <button type="submit" value="1" class="active button button-primary"><?php esc_html_e( 'Allow & Continue', 'shipping-manager' ); ?></button>
         </form>
     </div>
 </div>
