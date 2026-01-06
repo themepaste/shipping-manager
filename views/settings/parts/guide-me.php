594263387610596382bca1f55559d2cd3c0bbeb1
byteshake-habib <habib@themepaste.com>
2026-01-06 15:01:55 +0600

    misc

--- views/settings/parts/guide-me.php
+++ views/settings/parts/guide-me.php
@@ -6,27 +6,7 @@ defined( 'ABSPATH' ) || exit;
 $current_screen = $args['current_screen'];
 
 // Determine the corresponding documentation URL based on the current screen
-switch ( $current_screen ) {
-case 'shipping-fees':
-    $doc_url = 'https://themepaste.com/product-name/shipping-manager/';
-    break;
-case 'box-shipping':
-    $doc_url = 'https://themepaste.com/documentation/shipping-manager-documentation/setup-box-size-shipping';
-    break;
-case 'free-shipping':
-    $doc_url = 'https://themepaste.com/documentation/shipping-manager-documentation/set-up-free-shipping';
-    break;
-case 'shipping-calculator':
-    $doc_url = 'https://themepaste.com/documentation/shipping-manager-documentation/shipping-calculator';
-    break;
-case 'general':
-    $doc_url = 'https://themepaste.com/documentation/shipping-manager-documentation';
-    break;
-default:
-    // Fallback to base documentation if no matching section
-    $doc_url = 'https://themepaste.com/product-name/shipping-manager/';
-    break;
-}
+$doc_url = 'https://themepaste.com/documentation/shipping-manager/';
 ?>
 
 <!-- Button linking to the relevant documentation section -->
--- views/settings/parts/topbar.php
+++ views/settings/parts/topbar.php
@@ -27,7 +27,7 @@ printf( '<img src="%1$s" >', esc_url( $tpsm_icon ) );
     <!-- Right-aligned topbar info area -->
     <div class="tpsm-topbar-info-area">
         <!-- Link to plugin documentation -->
-        <a href="https://themepaste.com/product-name/shipping-manager/" target="_blank">
+        <a href="https://themepaste.com/documentation/shipping-manager/" target="_blank">
             <?php esc_html_e( 'Documentation', 'shipping-manager' ); ?>
         </a>
     </div>
